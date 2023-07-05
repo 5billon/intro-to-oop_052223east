b3571e238298b07970c5cb1cd11b88bd35ea212f
Kevinmrozek <kmrozek0859@gmail.com>
2023-07-05 10:53:36 -0500

    updated lecture stuff

--- lib/classes.py
+++ lib/classes.py
@@ -1,2 +1,27 @@
 class Cat:
-    pass
+    # This special method runs everytime an instance is created
+    def __init__(self, name_given, age_given):
+        self.name = name_given
+        self._age = age_given
+    
+    def get_age(self):
+        return self._age
+    
+    def set_age(self, new_age):
+        if new_age > 30:
+            print('cats dont live that long!')
+        else:
+            self._age = new_age
+
+    age = property(get_age, set_age)
+
+    
+    # an instance method, a method we can call on an instance
+    # e.g. c1.meow()
+    @property
+    def meow(self):
+        print(f'I am {self.name}, mee-yaow')
+
+
+    def __repr__(self):
+        return f'<Cat name = {self.name} age = {self.age}>'
--- run.py
+++ run.py
@@ -1,5 +1,14 @@
 import ipdb
 from lib.classes import Cat
 
+c1 = Cat('Luke', 5)
+
+c2 = Cat('Murphy', 3)
+
+#the_cats = []
+
+
+#for n in range(500):
+#   the_cats.append(Cat())
 
 ipdb.set_trace()
